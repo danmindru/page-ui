e133c7fc8cd294fa0da80f3b23c58422d72832ff
danmindru <hey@danmindru.dev>
2024-05-02 16:16:13 +0000

    🔄 Sync Landing Page components

--- components/blog/WrappedVideoPlayer.tsx
+++
@@ -1,5 +0,0 @@
-import { VideoPlayer } from '@/components/shared/VideoPlayer';
-
-export const WrappedVideoPlayer = (props) => {
-  return <VideoPlayer {...props} />;
-};
--- components/shared/Image.tsx
+++
@@ -1,5 +0,0 @@
-import NextImage, { ImageProps } from 'next/image';
-
-const Image = ({ ...rest }: ImageProps) => <NextImage {...rest} />;
-
-export default Image;
