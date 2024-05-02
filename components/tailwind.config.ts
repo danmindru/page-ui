c1f86a9bfdaa519d19a6d4ec84190e61362b3e06
anshul360 <got.trapped.in.the.matrix@gmail.com>
2024-05-02 23:03:15 +0530

    Update tailwind.config.ts

--- components/tailwind.config.ts
+++ components/tailwind.config.ts
@@ -7,6 +7,8 @@ const config: Config = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
