6a2344d37e48b625b21101eddd01832587284200
Dan Mindru <mindrudan@gmail.com>
2024-05-03 08:49:29 +0200

    Add temporary PH header

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -10,63 +10,83 @@ import Image from 'next/image';
 
 const Header = ({ className }: { className?: string }) => {
   return (
-    <header
-      className={cn(
-        'flex items-center justify-between py-10 flex-wrap w-full mb-20 lg:mb-32 pt-6 p-6 container-wide',
-        className
-      )}
-    >
-      <div>
-        <Link href="/" aria-label={siteConfig.logoTitle}>
-          <div className="flex items-center gap-3 justify-between">
-            <Image
-              src="/static/images/logo.png"
-              alt="Page UI logo"
-              height={47}
-              width={47}
-              className="group-hover:animate-wiggle "
-            />
+    <>
+      <div className="w-full gap-4 flex items-center justify-center bg-purple-950 p-2">
+        <a
+          href="https://www.producthunt.com/posts/page-ui?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-page&#0045;ui"
+          target="_blank"
+          className="flex gap-4 items-center justify-center opacity-100 hover:opacity-90 transition-all"
+        >
+          <p className="text-white">
+            We are live on Product Hunt today! Come by and say hi 🚀
+          </p>
 
-            <div className="hidden text-2xl font-semibold sm:flex h-full">
-              Page UI
-            </div>
-          </div>
-        </Link>
+          <img
+            src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=453266&theme=light"
+            alt="Page&#0032;UI - React&#0032;components&#0032;to&#0032;make&#0032;a&#0032;pretty&#0032;landing&#0032;page&#0032;that&#0032;converts | Product Hunt"
+            className="h-10 w-auto"
+          />
+        </a>
       </div>
-      <div className="flex items-center leading-5 gap-4 sm:gap-6">
-        {headerNavLinks.map((link) => {
-          if (link.href.includes('shipixen.com')) {
+
+      <header
+        className={cn(
+          'flex items-center justify-between py-10 flex-wrap w-full mb-20 lg:mb-32 pt-6 p-6 container-wide',
+          className,
+        )}
+      >
+        <div>
+          <Link href="/" aria-label={siteConfig.logoTitle}>
+            <div className="flex items-center gap-3 justify-between">
+              <Image
+                src="/static/images/logo.png"
+                alt="Page UI logo"
+                height={47}
+                width={47}
+                className="group-hover:animate-wiggle "
+              />
+
+              <div className="hidden text-2xl font-semibold sm:flex h-full">
+                Page UI
+              </div>
+            </div>
+          </Link>
+        </div>
+        <div className="flex items-center leading-5 gap-4 sm:gap-6">
+          {headerNavLinks.map((link) => {
+            if (link.href.includes('shipixen.com')) {
+              return (
+                <a
+                  key={link.title}
+                  href={link.href}
+                  className="nav-link hidden sm:block"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <span className="bg-clip-text bg-gradient-to-r text-transparent from-primary-400 to-secondary-300">
+                    {link.title}
+                  </span>
+                </a>
+              );
+            }
+
             return (
-              <a
+              <ActiveLink
                 key={link.title}
                 href={link.href}
                 className="nav-link hidden sm:block"
-                target="_blank"
-                rel="noopener noreferrer"
+                activeClassName="nav-link-active"
               >
-                <span className="bg-clip-text bg-gradient-to-r text-transparent from-primary-400 to-secondary-300">
-                  {link.title}
-                </span>
-              </a>
+                <span>{link.title}</span>
+              </ActiveLink>
             );
-          }
-
-          return (
-            <ActiveLink
-              key={link.title}
-              href={link.href}
-              className="nav-link hidden sm:block"
-              activeClassName="nav-link-active"
-            >
-              <span>{link.title}</span>
-            </ActiveLink>
-          );
-        })}
-        <SearchButton />
-        <ThemeSwitch />
-        <MobileNav />
-      </div>
-    </header>
+          })}
+          <SearchButton />
+          <ThemeSwitch />
+          <MobileNav />
+        </div>
+      </header>
+    </>
   );
 };
 
