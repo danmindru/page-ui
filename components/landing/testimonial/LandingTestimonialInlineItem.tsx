960b7adf3ca1fbd81ade4d76e5eb292ec848b87b
danmindru <hey@danmindru.dev>
2024-07-26 20:56:56 +0000

    🔄 Sync Landing Page components

--- components/landing/testimonial/LandingTestimonialInlineItem.tsx
+++ components/landing/testimonial/LandingTestimonialInlineItem.tsx
@@ -15,7 +15,7 @@ export const LandingTestimonialInlineItem = ({
   suffix,
 }: {
   className?: string;
-  imageSrc: string;
+  imageSrc?: string;
   text: string;
   name: string;
   suffix?: string;
@@ -23,14 +23,16 @@ export const LandingTestimonialInlineItem = ({
   return (
     <div className={clsx('flex flex-col text-center', className)}>
       <div className="flex gap-2 items-center justify-center">
-        <div className="opacity-90 flex-shrink-0">
-          <LandingAvatar
-            imageSrc={imageSrc}
-            name={name}
-            size="large"
-            className="rounded-full border-2 border-opacity-75"
-          />
-        </div>
+        {imageSrc ? (
+          <div className="opacity-90 flex-shrink-0">
+            <LandingAvatar
+              imageSrc={imageSrc}
+              name={name}
+              size="large"
+              className="rounded-full border-2 border-opacity-75"
+            />
+          </div>
+        ) : null}
 
         <div className="flex flex-col items-center text-xs truncate">
           <LandingRating rating={5} />
