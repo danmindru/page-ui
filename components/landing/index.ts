f36b0a1d943ae2c98331dfc9fc8714b473c90ea7
danmindru <hey@danmindru.dev>
2025-03-09 08:15:30 +0000

    🔄 Sync Landing Page components

---
+++ components/landing/index.ts
@@ -0,0 +1,49 @@
+export { LandingReadMoreWrapper } from './LandingReadMoreWrapper';
+export { LandingSocialProofBandItem } from './social-proof/LandingSocialProofBandItem';
+export { LandingTestimonialInline } from './testimonial/LandingTestimonialInline';
+export { LandingFeatureList } from './feature/LandingFeatureList';
+export { LandingFeature } from './feature/LandingFeature';
+export { LandingFaqSection } from './LandingFaq';
+export { LandingRating } from './rating/LandingRating';
+export { LandingProductVideoFeature } from './LandingProductVideoFeature';
+export { LandingSocialProof } from './social-proof/LandingSocialProof';
+export { LandingAvatar } from './social-proof/LandingAvatar';
+export { LandingProductHuntAward } from './social-proof/LandingProductHuntAward';
+export { LandingProductTourSection } from './LandingProductTour';
+export { LandingTestimonial } from './testimonial/LandingTestimonial';
+export { LandingDiscount } from './discount/LandingDiscount';
+export { LandingProductFeatureKeyPoints } from './LandingProductFeatureKeyPoints';
+export { LandingProductFeature } from './LandingProductFeature';
+export { LandingTestimonialInlineItem } from './testimonial/LandingTestimonialInlineItem';
+export { LandingTestimonialReadMoreWrapper } from './testimonial/LandingTestimonialReadMoreWrapper';
+export { LandingProductFeaturesGrid } from './LandingProductFeaturesGrid';
+export { LandingShowcaseItem } from './showcase/LandingShowcaseItem';
+export { LandingShowcase } from './showcase/LandingShowcase';
+export { LandingMarquee } from './LandingMarquee';
+export { LandingBandSection } from './LandingBand';
+export {
+  LandingProductTour,
+  LandingProductTourList,
+  LandingProductTourTrigger,
+  LandingProductTourContent,
+} from './LandingProductTour';
+export { LandingTestimonialGrid } from './testimonial/LandingTestimonialGrid';
+export { LandingTestimonialListSection } from './testimonial/LandingTestimonialList';
+export { LandingSocialProofBand } from './social-proof/LandingSocialProofBand';
+export { LandingFaqCollapsibleSection } from './LandingFaqCollapsible';
+export {
+  LandingPrimaryImageCtaSection,
+  LandingPrimaryVideoCtaSection,
+  LandingPrimaryTextCtaSection,
+} from './cta/LandingPrimaryCta';
+
+export { LandingSaleCtaSection } from './cta/LandingSaleCta';
+export { LandingHeader } from './navigation/LandingHeader';
+export { LandingHeaderMenuItem } from './navigation/LandingHeaderMenuItem';
+export { LandingFooter } from './footer/LandingFooter';
+export { LandingFooterColumn } from './footer/LandingFooterColumn';
+export { LandingFooterLink } from './footer/LandingFooterLink';
+export { LandingPricingPlan } from './pricing/LandingPricingPlan';
+export { LandingPricingSection } from './pricing/LandingPricingSection';
+export { LandingNewsletterSection } from './newsletter/LandingNewsletterSection';
+export { LandingNewsletterInput } from './newsletter/LandingNewsletterInput';
