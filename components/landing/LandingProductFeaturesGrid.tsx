15c28b76864b5e6729015708e2264373709a6048
danmindru <hey@danmindru.dev>
2025-03-11 13:55:42 +0000

    🔄 Sync Landing Page components

--- components/landing/LandingProductFeaturesGrid.tsx
+++ components/landing/LandingProductFeaturesGrid.tsx
@@ -100,7 +100,7 @@ export const LandingProductFeaturesGrid = ({
       <div
         className={clsx(
           '!p-0 relative isolate grid md:grid-cols-2 gap-4',
-          `${containerType}-container`,
+          `container-${containerType}`,
         )}
       >
         {childrenWithBackground}
